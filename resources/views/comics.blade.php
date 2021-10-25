b4232e7696c1e032999831df7b6b3616d6769aa3
WalterFrongia <walter.frongia@libero.it>
2021-10-25 18:35:18 +0200

    Impostato layout e impostati i components

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <h1>comics</h1>
+
+@endsection
---
+++ resources/views/movies.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.main")
+
+@section("content")
+    <h1>movies</h1>
+@endsection
