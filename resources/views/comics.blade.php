defb3227bd6e860052e8444f85f1e6b241e33e2d
WalterFrongia <walter.frongia@libero.it>
2021-10-26 16:43:47 +0200

    Implementazione file comics.php per importare array con info per stamapre dinamicamente le cards. Impostato file comics blade con importazione tramite routes

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-
-    <h1>comics</h1>
-
+    <div class="content-section">
+        @foreach ($cards as $card)
+            <div id="card">
+                <ul>
+                    <li>
+                        <img src="{{$card ['thumb]}}" alt="">
+                        <a href="">{{$card ["title"]}}</a>
+                    </li>
+                </ul>
+            </div>
+        @endforeach
+    </div>
 @endsection
