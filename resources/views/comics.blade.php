bcc8ff9da89e3c61e4f4df6bb2377fdc5aa99a25
WalterFrongia <walter.frongia@libero.it>
2021-10-31 12:36:18 +0100

    Implementato un id "content-wrapping" in comics per dare uno bg color alla sezione del contenuto

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="content-section">
-        @foreach ($cards as $card)
-            <div id="card">
-                <ul>
-                    <li>
-                        <img src="{{$card ['thumb']}}" alt="">
-                        <a href="{{route('card',$loop -> index)}}">{{$card ["title"]}}</a>
-                    </li>
-                </ul>
-            </div>
-        @endforeach
+    <div id="content-wrapping">
+        <div class="content-section">
+            @foreach ($cards as $card)
+                <div id="card">
+                    <ul>
+                        <li>
+                            <img src="{{$card ['thumb']}}" alt="">
+                            <a href="{{route('card',$loop -> index)}}">{{$card ["title"]}}</a>
+                        </li>
+                    </ul>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
