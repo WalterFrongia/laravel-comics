e179acd897615d2e56412d6619b1827ea96924c3
WalterFrongia <walter.frongia@libero.it>
2021-11-01 12:10:29 +0100

    Implementati elemenenti in card, aggiunto prezzo, descrizione e img di banner, implementato tutto all'interno di un id html in card container e lavorato su card scss per dare una grafica

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -7,20 +7,34 @@
 
         <div id="content-info-card">
 
-            <div class="card-container">
+            <div id="card-container">
 
-                <img src="{{$card ['thumb']}}" alt="">
+                <img id="comic-img"src="{{$card ['thumb']}}" alt="">
                 <h1>{{$card['title']}}</h1>
-                <p>U.S. Price:{{$card['price']}}</p>
-                <p>{{$card['description']}}</p>
-            </div>
             
-            <div class="info-section">
+                <div id="container-banner-price-ceck">
+                    <div id="price-container">
+                        <p>U.S. Price: <span>{{$card['price']}}</span></p>
+                        <p>AVALIABLE</p>
+                    </div>
+                    <div id="check-container">
+                        <p>Check Availibility</p>
+                    </div>
+                </div>
+                <div id="description-container">
+                    <p>{{$card['description']}}</p>
+                </div>
+            </div>
+            <div id="container-banner-img">
+                <img id="img-banner" src="/images/adv.jpg" alt="">
+            </div>
+
+            <!-- <div class="info-section">
                 <p>Series: {{$card['type']}}</p>
                 @foreach ($card ["writers"] as $writer )
                     <p>Written by: {{$writer}} </p>
                 @endforeach
-            </div>
+            </div> -->
         </div>
     </div>
 @endsection
